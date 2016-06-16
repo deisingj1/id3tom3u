5d111337d4036cd442ad0339fd2834f655cfb3ff
deisingj1 <deisingj1@hawkmail.newpaltz.edu>
2016-06-15 23:18:56 -0400

    Initial Commit

---
+++ id3tom3u/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  id3tom3u
+//
+//  Created by Jesse Deisinger on 6/15/16.
+//  Copyright (c) 2016 Jesse Deisinger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
