d201b53f414bd794c181db5044d7def9078c5bc7
deisingj1 <deisingj1@hawkmail.newpaltz.edu>
2016-06-17 17:54:13 -0400

    Added command line switches

--- id3tom3u/main.cpp
+++ id3tom3u/main.cpp
@@ -7,9 +7,55 @@
 //
 
 #include <iostream>
+#include <taglib/tag.h>
+#include <taglib/fileref.h>
+#include <dirent.h>
+#include <fstream>
+
+struct args {
+    const char * ifName;
+    const char * ofName;
+    const char * append;
+};
+
+struct args getArgs(int argc, const char * argv[]) {
+    struct args a;
+    for(int i = 1; i < argc; i++) {
+        if(argv[i][0] == '-'){
+            switch(argv[i][1]) {
+                    case 'i':
+                        a.ifName = argv[i+1];
+                        break;
+                    case 'o':
+                        a.ofName = argv[i+1];
+                        break;
+                    case 'a':
+                        a.append = argv[i+1];
+                    default:
+                        break;
+                }
+        }
+    }
+    return a;
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    struct args a = getArgs(argc, argv);
+    DIR *dp;
+    struct dirent *ep;
+    dp = opendir(a.ifName);
+    if(dp != NULL) {
+            while ((ep = readdir(dp)))
+            puts(ep->d_name);
+        (void) closedir(dp);
+    }
+    else {
+        perror("Couldn't find directory");
+    }
+    
+    TagLib::FileRef f("test.mp3");
+    TagLib::String artist = f.tag()->album();
+    TagLib::uint trackno = f.tag()->track();
+    std::cout << artist << " track " << trackno << std::endl;
     return 0;
 }
